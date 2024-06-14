1c64d5e2ef7af8bff5934acf6c458fc43d814122
HirschMariia <151627098+HirschMariia@users.noreply.github.com>
2024-06-14 15:31:04 +0200

    Add files via upload

---
+++ HW_Gen_Tech_16_05.sql
@@ -0,0 +1,29 @@
+
+-- Создать таблицу друзей с использованием механизма валидации данных в SQL
+
+CREATE TABLE friendship (
+    friendship_id INT AUTO_INCREMENT PRIMARY KEY,
+    user1_id int NOT NULL,
+    user2_id int NOT NULL,
+    user3_id int NOT NULL,
+    created_at timestamp not null default current_timestamp,
+    foreign key(user1_id) references users(id),
+    foreign key(user2_id) references users(id)
+    )
+
+
+-- Добавить в таблицу друзей несколько записей
+
+INSERT INTO friendship (user1_id, user2_id, user3_id) VALUES
+(1, 3),
+(1, 2),
+(2, 3),
+(3, 4),
+(1, 4)
+
+-- Вывести сумму всех транзакций отдельно по каждой валюте (в упорядоченном виде)
+
+SELECT sum(amount) AS total_amount, currency FROM txs
+JOIN accounts ON txs.sender_account_id = accounts.id
+GROUP BY accounts.currency
+ORDER BY total_amount DESC
